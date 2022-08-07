62478ffe6387d4145c580db2de5cbd1c29f26d84
yajurvedtech <yajurved.tech@gmail.com>
2022-08-07 14:59:34 +0530

    adding calculator logic

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a, int b){
+       return a-b;
+    }
+
+    public int mul(int a, int b){
+        return a*b;
+    }
+}
